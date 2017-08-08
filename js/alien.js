8c9b848d3ffc3ee27ec306ef6431b4e34d6fa616
ArielAviad <aviadrel@gmail.com>
2017-08-08 04:49:32 +0300

    create alien prototype

---
+++ js/alien.js
@@ -0,0 +1,19 @@
+
+Alien  = function (game){
+      Phaser.Sprite.call(this, game, game.world.randomX, -30, 'invader');
+
+      var weapon = game.add.weapon(30,'bullet');
+      weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+      weapon.bulletSpeed = enemyBulletSpeed;
+      weapon.fireRate = 10;
+      weapon.trackSprite(alien,0,0);
+      this.weapon = weapon;
+};
+
+Alien.prototype = Object.create(Phaser.Sprite.prototype);
+
+Alien.prototype.constructor = Alien;
+
+Alien.prototype.update = function (){
+    this.weapon.fire();
+}
