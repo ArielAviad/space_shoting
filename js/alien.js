f0c1c37acf0242e9f9ac49e6315cc7ff131e32c3
ArielAviad <aviadrel@gmail.com>
2017-08-08 05:03:06 +0300

    add gitignore file

--- js/alien.js
+++ js/alien.js
@@ -1,12 +1,12 @@
 
-Alien  = function (game){
+Alien  = function (game, sprite){
       Phaser.Sprite.call(this, game, game.world.randomX, -30, 'invader');
 
       var weapon = game.add.weapon(30,'bullet');
       weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
       weapon.bulletSpeed = enemyBulletSpeed;
       weapon.fireRate = 10;
-      weapon.trackSprite(alien,0,0);
+      weapon.trackSprite(sprite,0,0);
       this.weapon = weapon;
 };
 
